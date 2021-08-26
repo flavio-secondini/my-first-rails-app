0e8b0ba4fd680189a9f4e8466fbc7fe7acac7e51
flavio-secondini <flavio.secondini@gmail.com>
2021-08-26 19:09:49 +0200

    Creazione sezione commenti

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#welcome'
+  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'welcome', to: 'home#welcome'
 end
